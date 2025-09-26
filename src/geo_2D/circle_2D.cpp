fbd9e1909cc24cdaa6cba26cece14134381266e9
HUANG He <hu4nghe@outlook.com>
2025-09-26 13:40:48 +0200

    fix a bug in circle::contains that may cause precision problem.

--- src/geo_2D/circle_2D.cpp
+++ src/geo_2D/circle_2D.cpp
@@ -1,4 +1,5 @@
 #include "geo_2D/circle_2D.h"
+#include "base/dbl_compare.h"
 namespace tools_2D 
 {
     tools_2D::circle::circle(tools_2D::point center, double radius) 
@@ -7,6 +8,6 @@ namespace tools_2D
     bool 
     tools_2D::circle::contains(const tools_2D::point& p) const
     { 
-        return center.distance(p) <= radius; 
+        return dbl_inf(center.distance(p), radius); 
     }
 } // namespace tools_2D
